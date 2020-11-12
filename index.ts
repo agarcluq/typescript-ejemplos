d99d491f64a3684e17110b06731ac4c31cf3fc62
agarcluq <agarcluq@everis.com>
2020-11-12 12:23:00 +0100

    ejemplos

--- index.ts
+++ index.ts
@@ -7,5 +7,5 @@ appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 //enum color{red=4,green,blue};
 //enum color{red=4,green=4,blue};
-enum color{red,green=2,blue};
+enum color{red=-5,green,blue};
 console.log(color)
